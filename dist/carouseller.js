5ee8e2c50ead1455543b1ebbe40c47d117655819
shrue348 <shure348@gmail.com>
2015-12-15 10:22:50 +0300

    Add touch-swipe scroll support

--- dist/carouseller.js
+++ dist/carouseller.js
@@ -1,15 +1,23 @@
+(function(l){ 
+	var M=Math,ce,cx,cy,dx,dy,b,f,i,m,s,t=function(e,i){m=i; i=e.touches; return{x:i[0].pageX,y:i[0].pageY,z:i.length}};
+	for(i in s={touchcancel:function(e){m=0},touchstart:function(e){b=t(e,0)},touchmove:function(e){f=t(e,1)},touchend:function(e){if(b.z>1)return; ce=l.createEvent('CustomEvent');  
+	ce.initCustomEvent(m?(M.max(dx=M.abs(cx=f.x-b.x),dy=M.abs(cy=f.y-b.y))>120?dx>dy?cx<0?'l':'r':cy<0?'u':'d':'fc'):'fc',true,true,b); e.target.dispatchEvent(ce);}
+}) l.addEventListener(i,s[i],false)})(document);
+
 var carousel = function(i, t) {
     'undefined' == typeof t && (t = 0),
 	this.obj = $(i), 
 	this.autoscrolldelay = t, 
-	$(this.obj).find('.carousel-button-left').on('click', this.left.bind(this)), 
-	$(this.obj).find('.carousel-button-right').on('click', this.right.bind(this)),
+	$(this.obj).find('.carousel-button-left').on('click, r', this.left.bind(this)), 
+	$(this.obj).find('.carousel-button-right').on('click, l', this.right.bind(this)),
+	$(this.obj).on('l', this.right.bind(this)),
+	$(this.obj).on('r', this.left.bind(this)),
 	$(this.obj).on('mouseenter', this.mousein.bind(this)), 
 	$(this.obj).on('mouseleave', this.mouseout.bind(this)), t && (this.interval = window.setInterval(this.right.bind(this), t)),
 	$(this.obj).find('.carousel-items').css('height', function(){
 		var max = 0;
 		$(this).children().each(function() {
-			max = Math.max( max, $(this).height() );
+			max = Math.max( max, $(this).outerHeight() );
 		});
 		return max;
 	});
