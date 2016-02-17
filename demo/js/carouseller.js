be8317fd2241e4a9ac476d07037b96fe74cbdd95
shrue348 <shure348@gmail.com>
2016-02-17 15:23:39 +0300

    fixes
    -js fixes
    -disable scroll if items count < than carousel can view

--- demo/js/carouseller.js
+++ demo/js/carouseller.js
@@ -13,6 +13,7 @@
 		left_btn = obj.find('.carousel-button-left'),
 		right_btn = obj.find('.carousel-button-right'),
 		list = obj.find('.carousel-items'),
+		wrapper = obj.find('.carousel-wrapper'),
 		count = obj.find('.carousel-block').length,
 		sens = options.sens,
 		touch, startX, endX, result, wrap, t, count, can_view,
@@ -38,52 +39,77 @@
 		
 		mousein = function(){
 			window.clearInterval(this.interval);
-			console.log('mouseIn');
 		},
 		mouseout = function(){
 			if (options.autoScrollDelay > 0) this.interval = window.setInterval(function(){ right('click')}, options.autoScrollDelay);
 			if (options.autoScrollDelay < 0) this.interval = window.setInterval(function(){ left('click')}, -options.autoScrollDelay);
-			console.log('mouseOut');
 		},
 
-		left = function(e){
+		left = function(e, result){
 			t = obj.find('.carousel-block').eq(-1).outerWidth(true);
+			can_view = Math.round( wrapper.width() / t)
 
-			if (e.type == 'touchend') {
-				list.animate({
-					left: '0px'
-				}, options.scrollSpeed, options.easing, function() {
-					this.stop = !1;
-				});
+			if ( count > can_view) {
+				if (e.type == 'touchend') {
+					list.animate({
+						left: '0px'
+					}, options.scrollSpeed, options.easing, function() {
+						this.stop = !1;
+					});
+				} else {
+					obj.find('.carousel-items .carousel-block').eq(-1).prependTo(list);
+					list.css({ left: '-'+t+'px' });
+					list.stop().animate({
+						left: '0px'
+					}, options.scrollSpeed, options.easing, function() {
+						this.stop = !1;
+					});
+				}
 			} else {
-				obj.find('.carousel-items .carousel-block').eq(-1).prependTo(list);
-				list.css({ left: '-'+t+'px' });
-				list.stop().animate({
-					left: '0px'
-				}, options.scrollSpeed, options.easing, function() {
-					this.stop = !1;
-				});
+				list.stop().animate({left: '10px'},100),
+				list.animate({left: '0'},100)
 			}
-			console.log('moveLeft')
 		},
 
-		right = function(e){
+		right = function(e, result){
 			t = obj.find('.carousel-block').eq(-1).outerWidth(true);
+			can_view = Math.round( wrapper.width() / t)
 
-			if (e.type == 'touchend') {
-				//console.log(-2*t+'px')
-				list.animate({
-					left: -2*t+'px'
-					
-				}, options.scrollSpeed, options.easing, function() {
-					obj.find('.carousel-items .carousel-block').eq(0).appendTo(list);
-					obj.find('.carousel-items .carousel-block').eq(0).appendTo(list);
-					list.css({ left: '0px' });
+			if ( count > can_view) {
+				if (e.type == 'touchend') {
+					//console.log(-2*t+'px')
+					list.animate({
+						left: -2*t+'px'
+						
+					}, options.scrollSpeed, options.easing, function() {
+						obj.find('.carousel-items .carousel-block').eq(0).appendTo(list);
+						obj.find('.carousel-items .carousel-block').eq(0).appendTo(list);
+						list.css({ left: '0px' });
 
-					this.stop = !1;
-				});
+						this.stop = !1;
+					});
+				} else {
+					list.stop().animate({
+						left: '-' + t + 'px'
+					}, options.scrollSpeed, options.easing, function() {
+						obj.find('.carousel-items .carousel-block').eq(0).appendTo(list);
+						list.css({ left: '0px' });
+						this.stop = !1;
+					});
+				}
+				console.log('moveRight moved')
 			} else {
-				list.stop().animate({
+				list.stop().animate({left: '-10px'},100),
+				list.animate({left: '0'},100)
+			}
+		},
+		
+		back = function(result){
+			t = obj.find('.carousel-block').eq(-1).outerWidth(true);
+			can_view = Math.round( wrapper.width() / t)
+			
+			if( result = 0) {
+				list.animate({
 					left: '-' + t + 'px'
 				}, options.scrollSpeed, options.easing, function() {
 					obj.find('.carousel-items .carousel-block').eq(0).appendTo(list);
@@ -91,46 +117,42 @@
 					this.stop = !1;
 				});
 			}
-			
-			console.log('moveRight')
-		},
-		
-		back = function(){
-			list.animate({
-				left: '-' + t + 'px'
-			}, options.scrollSpeed, options.easing, function() {
-				obj.find('.carousel-items .carousel-block').eq(0).appendTo(list);
-				list.css({ left: '0px' });
-				this.stop = !1;
-			});
 		},
 
 		touchstart = function(e) {
-			t = obj.find('.carousel-block').eq(-1).outerWidth(true),
-
-			obj.find('.carousel-items .carousel-block').eq(-1).prependTo(list);
-			list.css({ left: '-'+t+'px' });
-
-			touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
-			startX = touch.clientX
+			t = obj.find('.carousel-block').eq(-1).outerWidth(true);
+			can_view = Math.round( wrapper.width() / t);
+			result=0;
+			
+			if ( count > can_view ) {
+				obj.find('.carousel-items .carousel-block').eq(-1).prependTo(list);
+				list.css({ left: '-'+t+'px' });
+				touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+				startX = touch.clientX
+			}
 		},
 
 		touchmove = function(e) {
-			e.preventDefault();
-
+			t = obj.find('.carousel-block').eq(-1).outerWidth(true);
+			can_view = Math.round( wrapper.width() / t)
 			touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
-			endX = touch.clientX
-			result = startX - endX;
+			
+			if ( count > can_view) {
+				endX = touch.clientX
+				result = startX - endX;
+			} else {
+				result = 0;
+			}
 			
 			if (Math.abs(result) > 30 && Math.abs(result) < t){
+				e.preventDefault();
 				list.css({ left: '-'+t-result+'px' });
 			}
 		},
 
 		touchend = function(e) {
-			(result > sens) ? right(e) :  (result < -sens) ? left(e) : back();
+			(result > sens) ? right(e,result) :  (result < -sens) ? left(e,result) : back(result);
 		};
-		
 		return this.each(init);
 	}
 })(jQuery);
